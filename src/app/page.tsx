3ef1908222580c9d24bed6db59fbba8e845d9bf0
bala-emeng <47141828+bala-emeng@users.noreply.github.com>
2025-07-22 15:12:54 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 export default function Home() {
   return (
     <><div>
-    <div className="font-bold text-3xl">
+    <div className="text-center font-bold text-3xl">
     Litcoder AI Chat
     </div>
     <Page/>
